290682b5ebaabf5cb4dd6829dca11ec5a55dfcb5
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-11-07 19:49:23 -0300

    Implementando Classe PedidoAbono

--- 2 Entrega/gerenciamento.atestados/src/main/java/ufpe/cin/gerenciamento/atestados/entidades/PedidoAbono.java
+++ 2 Entrega/gerenciamento.atestados/src/main/java/ufpe/cin/gerenciamento/atestados/entidades/PedidoAbono.java
@@ -1,5 +1,49 @@
 package ufpe.cin.gerenciamento.atestados.entidades;
 
 public class PedidoAbono {
-    
+    private Funcionario funcionario;
+    private String date;
+    private String inicio;
+    private String fim;
+    private String justificativa;
+
+    public Funcionario getFuncionario() {
+        return this.funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getInicio() {
+        return this.inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFim() {
+        return this.fim;
+    }
+
+    public void setFim(String fim) {
+        this.fim = fim;
+    }
+
+    public String getJustificativa() {
+        return this.justificativa;
+    }
+
+    public void setJustificativa(String justificativa) {
+        this.justificativa = justificativa;
+    }
 }
